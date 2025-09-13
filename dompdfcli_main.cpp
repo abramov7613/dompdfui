2bc368b297279ac7fdffbc045858762c15bc7ebc
Vladimir Abramov <abramov7613@yandex.ru>
2025-09-13 21:46:36 +0300

    add --force-out option
    add multiple files input-output support

--- dompdfcli_main.cpp
+++ dompdfcli_main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <tuple>
 #include <filesystem>
 #include <stdexcept>
 #include <algorithm>
@@ -18,22 +19,25 @@ namespace po = boost::program_options;
 namespace fs = std::filesystem ;
 namespace nw = boost::nowide;
 
-int parse_cli_args(int argc, char** argv, po::variables_map& opts) ;
-void html2pdf(const po::variables_map& opts) ;
-void extract_embedded_resources(const po::variables_map& opts) ;
+
+// prototypes
+std::tuple<int, std::vector<fs::path>, std::vector<fs::path>, po::variables_map> parse_cli_args(int argc, char** argv) ;
+void extract_embedded_resources(const po::variables_map&) ;
+void html2pdf(const std::vector<fs::path>&, const std::vector<fs::path>&, const po::variables_map&) ;
 fs::path temp_path() ;
 
+
+// entry point
 int main(int argc, char** argv)
 {
   try {
     if(!nw::system(nullptr))
-      throw std::runtime_error("the command processor not exists");
+      throw std::runtime_error("the command processor is not exists");
     nw::args utf8_args (argc, argv);
-    po::variables_map opts;
-    auto parse_cli_args_result = parse_cli_args(argc, argv, opts) ;
-    if( parse_cli_args_result!=1 ) return parse_cli_args_result ;
+    auto [parse_result, in_files, out_files, opts] = parse_cli_args(argc, argv) ;
+    if( parse_result!=1 ) return parse_result ;
     extract_embedded_resources(opts);
-    html2pdf(opts);
+    html2pdf(in_files, out_files, opts);
     if(!opts["no-clean"].as<bool>()) fs::remove_all(temp_path());
   }
   catch (const std::exception& e) {
@@ -47,24 +51,29 @@ int main(int argc, char** argv)
   return 0;
 }
 
-// function parse_cli_args returns:
-//      1  - OK
-//      0  - Help
-//     -1  - Parse error
-int parse_cli_args(int argc, char** argv, po::variables_map& vm)
+
+// function return 4 values:
+//     first is a cli parser result: 1 = OK; 0 = Help; -1 = parser error
+//     second  - array of input files
+//     third   - array of output files
+//     fourth  - boost::program_options::variables_map container with all parameters
+std::tuple<int, std::vector<fs::path>, std::vector<fs::path>, po::variables_map> parse_cli_args(int argc, char** argv)
 {
   static const char* short_descr = "HTML to PDF Converter";
+  po::variables_map vm;
+  std::vector<fs::path> in_files, out_files;
+  fs::path out_dir;
   try {
     po::options_description hopts("Hidden options");
     hopts.add_options()
-        ("input-file", po::value<std::string>(), "input file")
-        ("output-file", po::value<std::string>(), "output file")
+        ("iofiles", po::value<std::vector<std::string>>(), "input output files")
         ;
 
     po::options_description popts("Program Options");
     popts.add_options()
         ("no-clean,n", po::bool_switch(), "don't clean temp files on exit")
         ("php-memory-limit,m", po::value<unsigned long long>()->default_value(268435456), "Limits the amount of memory (in bytes) a php-cli can use.")
+        ("force-out,f", po::bool_switch(), "replace output file if exists")
         ("help,h", "view this help message")
         ("version,v", "print version")
         ;
@@ -105,55 +114,75 @@ int parse_cli_args(int argc, char** argv, po::variables_map& vm)
     po::options_description allopts("");
     allopts.add(hopts).add(popts).add(dopts);
     po::positional_options_description p;
-    p.add("input-file", 1);
-    p.add("output-file", 1);
+    p.add("iofiles", -1);
     po::store(po::command_line_parser(argc, argv).options(allopts).positional(p).run(), vm);
     po::notify(vm);
 
     if (vm.count("help")) {
-        nw::cout <<  short_descr << "\nUsage:\t" << argv[0]
-                            << " [OPTIONS] INPUT-FILE OUTPUT-FILE \n"
+        nw::cout <<  short_descr << ' ' << git_tag_str << "\nUsage:\t" << argv[0]
+                            << " [OPTIONS] INPUT-FILE1 [INPUT-FILE2] [INPUT-FILE3] [...] OUTPUT-DIR \n"
                             << (po::options_description("").add(popts).add(dopts)) << '\n';
-        return 0;
+        return {0, {}, {}, {}};
     }
+
     if (vm.count("version")) {
-        nw::cout << short_descr << ' ' << git_tag_str << '\n'
-                 << "build time: " << build_time_str << '\n'
-                 << "git hash: " << git_hash_str << "\nusing:\n"
-                 << "GCC v" << __VERSION__ << '\n'
-                 << "boost v" << BOOST_VERSION / 100000 << '.'
-                              << BOOST_VERSION / 100 % 1000 << '.'
-                              << BOOST_VERSION % 100 << '\n'
-                 << "php-cli v" << PHPCLI_VERSION << '\n'
-                 << "dompdf v" << DOMPDF_VERSION << '\n' ;
-        return 0;
+        nw::cout << short_descr << ' ' << git_tag_str << "\nbuilt with: GCC " << __VERSION__
+                  << "; boost " << BOOST_VERSION / 100000 << '.' << BOOST_VERSION / 100 % 1000
+                  << '.' << BOOST_VERSION % 100 << "; php-cli " << PHPCLI_VERSION
+                  << "; dompdf " << DOMPDF_VERSION << "\nbuilt time: " << build_time_str << "\ngit hash: "
+                  << git_hash_str << '\n' ;
+        return {0, {}, {}, {}};
     }
-    if (!vm.count("input-file") || !vm.count("output-file")) {
-        nw::cout << "Error: the options 'INPUT-FILE' and 'OUTPUT-FILE' is required but missing\n";
-        return -1;
+
+    if (!vm.count("iofiles") || vm["iofiles"].as<std::vector<std::string>>().size()<2) {
+        nw::cout << "Error: the options 'INPUT-FILE1' and 'OUTPUT-DIR' is required but missing\n";
+        return {-1, {}, {}, {}};
     }
-    if( fs::path p { vm["input-file"].as<std::string>() }; !fs::exists(p) ){
-        nw::cout << "Error: file '" << p.string() << "' not found\n" ;
-        return -1;
+
+    for(const auto& e: vm["iofiles"].as<std::vector<std::string>>()) in_files.emplace_back(e);
+    std::transform(in_files.begin(), in_files.end(), in_files.begin(), [](auto& e){
+      return fs::absolute(e);
+    });
+    out_dir = in_files.back();
+    in_files.pop_back();
+    if(fs::is_regular_file(out_dir)) out_dir = out_dir.parent_path();
+    if(!fs::exists(out_dir)) fs::create_directory(out_dir);
+    if(!fs::is_directory(out_dir)) {
+        nw::cout << "Error: can't open output directory " << out_dir.string() << '\n' ;
+        return {-1, {}, {}, {}};
+    }
+    std::erase_if(in_files, [](const auto& e){
+      bool remove_element = !fs::exists(e);
+      if(remove_element) nw::cout << "Warning: file '" << e.string() << "' not found\n";
+      return remove_element;
+    });
+    if(in_files.empty()) return {-1, {}, {}, {}};
+
+    std::transform(in_files.begin(), in_files.end(), std::back_inserter(out_files), [&out_dir](const auto& e){
+      return out_dir / e.filename().replace_extension("pdf");
+    });
+
+    if(!vm["force-out"].as<bool>()){
+      bool already_exists = std::any_of(out_files.begin(), out_files.end(), [](const auto& e){
+        bool result = fs::exists(e);
+        if(result) nw::cout << "Warning: file '" << e.string() << "' already exists\n";
+        return result;
+      });
+      if( already_exists ) return {-1, {}, {}, {}};
     }
   }
   catch(const po::error& e) {
     nw::cout << "Error: " << e.what() << "\nTry:\t" << argv[0] << " --help\n";
-    return -1;
+    return {-1, {}, {}, {}};
   }
-  return 1;
+  return {1, in_files, out_files, vm};
 }
 
+
 // function generate php script from Options and run it
-void html2pdf(const po::variables_map& opts)
+void html2pdf(const std::vector<fs::path>& in_files, const std::vector<fs::path>& out_files, const po::variables_map& opts)
 {
   auto script_path = temp_path() / "html2pdf.php" ;
-  auto from_path = fs::path( opts["input-file"].as<std::string>() );
-  auto tmpfrom_path = temp_path() / from_path.filename() ;
-  auto to_path = fs::path( opts["output-file"].as<std::string>() );
-  auto tmpto_path = temp_path() / to_path.filename() ;
-  if(!fs::copy_file(from_path, tmpfrom_path, fs::copy_options::overwrite_existing))
-    throw std::runtime_error("Can't write to file: " + tmpfrom_path.string());
   nw::ofstream script( script_path ) ;
   if(!script.is_open()) throw std::runtime_error("Can't open file: " + script_path.string()) ;
   auto php_array = [](const std::vector<std::string>& v){
@@ -178,6 +207,7 @@ void html2pdf(const po::variables_map& opts)
   };
   script <<
     "<?php\n"
+    "if ($argc<3) exit(-1);\n\n"
     "require_once 'dompdf/autoload.inc.php';\n\n"
     "use Dompdf\\Dompdf;\n"
     "use Dompdf\\Options;\n\n"
@@ -233,33 +263,41 @@ void html2pdf(const po::variables_map& opts)
 
   script <<
     "\n$dompdf = new Dompdf($options);\n"
-    "$html_content = file_get_contents(\"" << tmpfrom_path.filename().string() << "\");\n"
+    "$html_content = file_get_contents(\"$argv[1]\");\n"
     "$dompdf->loadHtml($html_content);\n"
     "$dompdf->render();\n"
     "$output = $dompdf->output();\n"
-    "file_put_contents(\"" << tmpto_path.filename().string() << "\", $output);\n" ;
+    "if(file_put_contents(\"$argv[2]\", $output) === FALSE){\n"
+    "  echo \"Error: can't write to file: $argv[2]\" , PHP_EOL;\n"
+    "  exit(-1);\n"
+    "}\n" ;
   script.close();
+  nw::cout.flush();
 
+  for(size_t i=0; i<in_files.size(); ++i){
+    std::string ifile = in_files[i].string();
+    std::string ofile = out_files[i].string();
+    std::string cmd = "php.exe -c . \"" + script_path.filename().string() + "\" \""
+                      + ifile + "\" \"" + ofile + "\"" ;
 #if BOOST_OS_WINDOWS
-  std::string cmd = temp_path().root_name().string() + " && cd \"" + temp_path().string()
-                    + "\" && php.exe -c . \"" + script_path.filename().string() + "\"";
+    cmd = temp_path().root_name().string() + " && cd \"" + temp_path().string() + "\" && " + cmd ;
 #else
-  std::string cmd = "cd \"" + temp_path().string() + "\" && ./php.exe -c . \""
-                    + script_path.filename().string() + "\"";
+    cmd = "cd \"" + temp_path().string() + "\" && ./" + cmd ;
 #endif
-  if( nw::system(cmd.c_str()) )
-    throw std::runtime_error("Can't execute script file: " + script_path.string());
-
-  if(!fs::copy_file(tmpto_path, to_path, fs::copy_options::overwrite_existing))
-    throw std::runtime_error("Can't write to file: " + to_path.string());
+    if( nw::system(cmd.c_str()) )
+      throw std::runtime_error("Can't execute '" + script_path.filename().string() + "' with files:\n\t"
+                                + ifile + "\n\t" + ofile + '\n');
+  }
 }
 
-// function return temp path
+
+// function return application specific temp path
 fs::path temp_path()
 {
   std::string dir_name = "dompdfui_" + std::string(git_hash_str);
   return fs::temp_directory_path() / dir_name ;
 }
+
 
 // function extract embedded resources
 void extract_embedded_resources(const po::variables_map& opts)
@@ -309,6 +347,7 @@ void extract_embedded_resources(const po::variables_map& opts)
         "   exit(-1);\n"
         "}\n" ;
       unzipscript.close();
+      nw::cout.flush();
 #if BOOST_OS_WINDOWS
       std::string cmd = temp_path().root_name().string() + " && cd \""
                         + temp_path().string() + "\" && php.exe unzip.php";
